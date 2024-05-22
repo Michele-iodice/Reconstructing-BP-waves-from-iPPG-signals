bdb1ac6336fecdede469a162619de8fd3beade54
Michele Iodice <M.iodice21@studenti.unisa.it>
2024-05-22 22:19:15 +0200

    coding into pytorch framework

--- scripts/python/test/predict.py
+++ scripts/python/test/predict.py
@@ -1,54 +1,97 @@
 import numpy as np
-import tensorflow as tf
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
 import scipy.io
-
-from tensorflow.python.keras.optimizers import Adam
-from tensorflow.keras.models import model_from_json
-
-
-# Disable eager execution (Adam optimizer cannot be used if this option is enabled)
-tf.compat.v1.disable_eager_execution()
-tf.executing_eagerly()
-
-# Run over a specific GPU
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
+import json
 
 # GLOBAL VARIABLES
-VERBOSE = 2
+VERBOSE = True
 BATCH_SIZE = 16
+LEARNING_RATE = 1e-3
 
 
-# LOAD MODEL
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-model.load_weights('weights.h5')
-model.compile(optimizer=Adam(lr=1e-3), loss='mean_squared_error')
-model.summary()
+class ChannelAdaptation(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ChannelAdaptation, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
 
 
-# LOAD TEST DATA
-data = scipy.io.loadmat('data_test.mat')
+def load_model_from_json(json_path, weights_path, input_channels=2):
+    with open(json_path, 'r') as json_file:
+        model_architecture = json.load(json_file)
 
-xtest = np.zeros((data['CWT_ppg_test'].shape[1], data['CWT_ppg_test'][0,0]['cfs'][0,0].shape[0], data['CWT_ppg_test'][0,0]['cfs'][0,0].shape[1],2))
-ytest = np.zeros((data['CWT_bp_test'].shape[1], data['CWT_bp_test'][0,0]['cfs'][0,0].shape[0], data['CWT_bp_test'][0,0]['cfs'][0,0].shape[1],2))
+    model = nn.Sequential()
+    for layer in model_architecture['layers']:
+        layer_type = layer['type']
+        if layer_type == 'Conv2d':
+            model.add_module(layer['name'], nn.Conv2d(**layer['params']))
+        elif layer_type == 'ReLU':
+            model.add_module(layer['name'], nn.ReLU(**layer['params']))
+        # Add other layer types as needed
 
-for i in range(data['CWT_ppg_test'].shape[1]):
-    xtest[i,:,:,0] = np.real(data['CWT_ppg_test'][0,i]['cfs'][0,0])
-    xtest[i,:,:,1] = np.imag(data['CWT_ppg_test'][0,i]['cfs'][0,0])
-    ytest[i,:,:,0] = np.real(data['CWT_bp_test'][0,i]['cfs'][0,0])
-    ytest[i,:,:,1] = np.imag(data['CWT_bp_test'][0,i]['cfs'][0,0])
+    # Add channel adaptation if input channels != model input channels
+    channel_adapter = ChannelAdaptation(input_channels, model_architecture['input_channels'])
+    model = nn.Sequential(channel_adapter, model)
+
+    model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+    return model
 
 
-results = np.zeros((ytest.shape[0]), dtype=np.object)
+def load_data(file_path, variable_names):
+    data = scipy.io.loadmat(file_path)
+    data_dict = {}
+    for var in variable_names:
+        var_data = data[var]
+        shaped_data = np.zeros(
+            (var_data.shape[1], var_data[0, 0]['cfs'][0, 0].shape[0], var_data[0, 0]['cfs'][0, 0].shape[1], 2))
+        for i in range(var_data.shape[1]):
+            shaped_data[i, :, :, 0] = np.real(var_data[0, i]['cfs'][0, 0])
+            shaped_data[i, :, :, 1] = np.imag(var_data[0, i]['cfs'][0, 0])
+        data_dict[var] = shaped_data
+    return data_dict
 
-for i in range(ytest.shape[0]):
-    pred = model.predict(np.expand_dims(xtest[i],0))
-    results[i] = {}
-    results[i]['prediction'] = pred[0]
 
-scipy.io.savemat('results.mat',{'results':results})
+def create_dataloader(x, y):
+    x_tensor = torch.tensor(x, dtype=torch.float32)
+    y_tensor = torch.tensor(y, dtype=torch.float32)
+    dataset = TensorDataset(x_tensor, y_tensor)
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
+    return dataloader
+
+
+def predict_and_save_results(model, test_loader, device, output_file):
+    model.eval()
+    results = []
+
+    with torch.no_grad():
+        for xb, _ in test_loader:
+            xb = xb.to(device)
+            preds = model(xb)
+            for pred in preds:
+                results.append({'prediction': pred.cpu().numpy()})
+
+    scipy.io.savemat(output_file, {'results': results})
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load model
+model = load_model_from_json('model.json', 'weights.pth')
+model = model.to(device)
+
+# Load test data
+data = load_data('data_test.mat', ['CWT_ppg_test', 'CWT_bp_test'])
+xtest, ytest = data['CWT_ppg_test'], data['CWT_bp_test']
+
+# Create dataloader
+test_loader = create_dataloader(xtest, ytest)
+
+# Predict and save results
+predict_and_save_results(model, test_loader, device, 'results.mat')
+
 
 
